3a9d35d14583578f5ca2be4715a4b08d7dd016dc
varsha-vish <varshavish48@gmail.com>
2023-12-22 18:24:52 +0530

    modified files

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: process.env.NODE_ENV === "production" ? "/GS-Hydro-Tech/" : "/",
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader

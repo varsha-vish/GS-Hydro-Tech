99bde96da8c87dcfdb3627743a313ae7ca56db28
varsha-vish <varshavish48@gmail.com>
2023-12-24 19:15:13 +0530

    updated dist

--- dist/js/about.aceca748.js
+++ dist/js/about.aceca748.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkwebsite"]=self["webpackChunkwebsite"]||[]).push([[443],{6506:function(e,n,t){t.r(n),t.d(n,{default:function(){return f}});var s=t(3396);const u={class:"about"},a=(0,s._)("h1",null,"This is an about page",-1),r=[a];function i(e,n){return(0,s.wg)(),(0,s.iD)("div",u,r)}var c=t(89);const o={},b=(0,c.Z)(o,[["render",i]]);var f=b}}]);
-//# sourceMappingURL=about.66bc9bcf.js.map
+//# sourceMappingURL=about.aceca748.js.map
